66bb4b26e891f8f0ce0b9d1ce4919ca6c48d5196
Fernando Salas <luisfernandosalasg@gmail.com>
2025-05-24 19:46:10 -0500

    behavioral design pattern / command

---
+++ src/main/java/org/fernando/behavioral/command/Command.java
@@ -0,0 +1,5 @@
+package org.fernando.behavioral.command;
+
+public interface Command {
+    void execute();
+}
---
+++ src/main/java/org/fernando/behavioral/command/CommandApp.java
@@ -0,0 +1,19 @@
+package org.fernando.behavioral.command;
+
+public class CommandApp {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        RemoteControl remoteControl = new RemoteControl();
+
+        // Turn the light on
+        remoteControl.setCommand(lightOn);
+        remoteControl.pressButton();
+
+        // Turn the light off
+        remoteControl.setCommand(lightOff);
+        remoteControl.pressButton();
+    }
+}
